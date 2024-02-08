b08a3d576ceb91bf780083c2316170ba0be02c8f
Yaroslav Blokha <ipz213_byas@student.ztu.edu.ua>
2024-02-08 23:44:08 +0200

    Fixed main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,19 @@ import { Button } from 'antd';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.center}>
-        <div className={styles.banner}>
-          <div className={styles.imageContainer}>
-            <Image
-              src="/next.svg"
-              alt="Next.js Logo"
-              width={180}
-              height={100}
-              priority
-            />
-          </div>
-          <div className={styles.buttonContainer}>
-            <Button type="primary">Button</Button>
-          </div>
+    <div className={styles.container}>
+      <Image
+        src="/quiz.jpg"
+        alt="Background image"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+      />
+      <div className={styles.content}>
+        <div className={styles.center}>
+          <Button type="primary" size="large">Розпочати тест</Button>
         </div>
       </div>
-    </main>
+    </div>
   );
 }

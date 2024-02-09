926ffef3e01dd01126084303f32c9c08f32b322a
Yevhenii Biletskyi <biletskijzena@protonmail.com>
2024-02-09 21:04:01 +0200

    created resul component

---
+++ src/components/Results/index.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { Typography, Button } from 'antd'
+import TeamsTable from '../TeamTable'
+import { IRound } from '@/models/Round'
+import { ITeam } from '@/models/Team'
+import ResultTable from '../ResultTable'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from '@/modules/store/store'
+import { useRouter } from 'next/navigation'
+import { updateTeam } from '@/modules/store/reducers/teamSlice'
+
+const { Title } = Typography
+
+function Results() {
+  const router = useRouter()
+  const gameData = useSelector((state: RootState) => state.game)
+
+  if (gameData.gameIsStarted) {
+    //change route to game route
+    router.push('/')
+  }
+
+  const round1: IRound = {
+    roundNumber: 1,
+    roundPoints: 20,
+  }
+
+  const round2: IRound = {
+    roundNumber: 2,
+    roundPoints: 15,
+  }
+
+  const round3: IRound = {
+    roundNumber: 3,
+    roundPoints: 25,
+  }
+
+  // Mock data for ITeam interface
+  const team: ITeam = {
+    id: 1,
+    name: 'Team A',
+    rounds: [round1, round2, round3],
+    points: 60,
+  }
+
+  const handleAddtoTeam = () => {
+    if (gameData.team) {
+      const team : ITeam = gameData.team
+      useDispatch(updateTeam(team))
+    }
+  }
+
+  return (
+    <div>
+      <Title level={2}>Результати гри</Title>
+      <Button>Додати результат</Button>
+      <TeamsTable teamData={team} />
+      <Title level={2}>Результати інших команд</Title>
+      <TeamsTable setPagination={true} />
+    </div>
+  )
+}
+
+export default Results

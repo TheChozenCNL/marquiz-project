191b076c51579584d6cafd6cc345377690a8cc83
Yevhenii Biletskyi <biletskijzena@protonmail.com>
2024-02-15 10:44:22 +0200

    Added cheker for empty inputs

--- src/components/AddCategory/index.tsx
+++ src/components/AddCategory/index.tsx
@@ -2,37 +2,52 @@ import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '@/modules/store/store'
 import { addCategory } from '@/modules/store/reducers/categorySlice'
-import { Button, Input, Modal } from 'antd'
+import { Button, Input, Modal, message } from 'antd'
 import s from './style.module.scss'
 
 const AddCategorySection = () => {
   const [categoryName, setCategoryName] = useState<string>('')
   const [modalVisible, setModalVisible] = useState<boolean>(false)
   const dispatch = useDispatch()
-  const categories = useSelector((state: RootState) => state.categories?.categories ?? [])
+  const categories = useSelector(
+    (state: RootState) => state.categories?.categories ?? []
+  )
   const categoriesLength = categories.length
   const showAddButton = categoriesLength < 6
   const errorMessage = 'Мінімум 4 категорії потрібно'
 
   const handleAddCategory = () => {
-    dispatch(addCategory({
-      id: (categoriesLength ?? 0) + 1,
-      name: categoryName,
-    }));
+    if (categoryName === '' || categoryName === undefined) {
+      message.error('Будь ласка, введіть назву категорії')
+      return
+    }
+
+    dispatch(
+      addCategory({
+        id: (categoriesLength ?? 0) + 1,
+        name: categoryName,
+      })
+    )
     setCategoryName('')
     setModalVisible(false)
   }
 
   return (
     <>
-      {showAddButton && <Button onClick={() => setModalVisible(true)}>Додати категорію</Button>}
+      {showAddButton && (
+        <Button onClick={() => setModalVisible(true)}>Додати категорію</Button>
+      )}
       <Modal
         title="Додати категорію"
         open={modalVisible}
         onCancel={() => setModalVisible(false)}
         footer={[
-          <Button key="cancel" onClick={() => setModalVisible(false)}>Відміна</Button>,
-          <Button key="submit" type="primary" onClick={handleAddCategory}>Додати</Button>,
+          <Button key="cancel" onClick={() => setModalVisible(false)}>
+            Відміна
+          </Button>,
+          <Button key="submit" type="primary" onClick={handleAddCategory}>
+            Додати
+          </Button>,
         ]}
       >
         <div className={s.categoryInput}>
@@ -41,7 +56,9 @@ const AddCategorySection = () => {
             value={categoryName}
             onChange={(e) => setCategoryName(e.target.value)}
           />
-          {categoriesLength < 4 && <div style={{ color: 'red' }}>{errorMessage}</div>}
+          {categoriesLength < 4 && (
+            <div style={{ color: 'red' }}>{errorMessage}</div>
+          )}
         </div>
       </Modal>
     </>
